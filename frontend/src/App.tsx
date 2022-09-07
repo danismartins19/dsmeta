091bf959838f5933e487cda3a6a1ea03af72a80d
Daniel Martins <danidiamoo1@gmail.com>
2022-09-07 15:05:59 -0300

    other componenets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 const App = ()=> {
   return (
     <>
+      <Header/>
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+              <SalesCard />
+          </div>
+        </section>
+      </main>
       <NotificationButton/>
     </>
   )
