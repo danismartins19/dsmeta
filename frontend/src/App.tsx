4b2f196af392b93019e1e07e6642898bf40da514
Daniel Martins <danidiamoo1@gmail.com>
2022-09-20 21:40:50 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
 
 const App = ()=> {
   return (
     <>
+      <ToastContainer/>
       <Header/>
-
       <main>
         <section id="sales">
           <div className="dsmeta-container">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/baseUrl'
 import './styles.css'
@@ -11,7 +12,9 @@ const NotificationButton = ({saleId} : Props)  => {
 
     const handleNotification = (saleId : number) => {
         axios.get(`${BASE_URL}/sales/${saleId}/notification`)
-        .then(()=> console.log("Enviado"))
+        .then(()=> {
+            toast.info("SMS enviado com sucesso!");
+        })
     }
 
     return (
